f09ad0a0d69c0810cdc56d70f6bb74f310f3f5ab
Pedro Padilha Neto <pedropadilhan@gmail.com>
2023-03-07 13:58:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TargetFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número positivo inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (EhFibonacci(numero))
+            {
+                Console.WriteLine(numero + " pertence à sequência de Fibonacci!");
+            }
+            else
+            {
+                Console.WriteLine(numero+ " não pertence à sequência de Fibonacci.");
+            }
+        }
+
+        static bool EhFibonacci(int numero)
+        {
+            int a = 0, b = 1;
+
+            while (b < numero)
+            {
+                int guarda = a;
+                a = b;
+                b = guarda + b;
+            }
+
+            return b == numero;
+        }
+    }
+    
+}
